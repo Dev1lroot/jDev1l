2b1cd6e8f03cb5268cfdd945e5da1fc8ddfa0289
Einar Aldersen <46210204+Dev1lroot@users.noreply.github.com>
2019-04-13 14:40:41 +0200

    Create Colors.java

---
+++ src/no/dev1lroot/lib/Colors.java
@@ -0,0 +1,43 @@
+package no.dev1lroot.lib;
+
+public class Colors {
+    public static class fgColor{
+        public static String Black = "\u001b[30m";
+        public static String Red = "\u001b[31m";
+        public static String Green = "\u001b[32m";
+        public static String Yellow = "\u001b[33m";
+        public static String Blue = "\u001b[34m";
+        public static String Magenta = "\u001b[35m";
+        public static String Cyan = "\u001b[36m";
+        public static String White = "\u001b[37m";
+
+        public static String LightBlack = "\u001b[30;1m";
+        public static String LightRed = "\u001b[31;1m";
+        public static String LightGreen = "\u001b[32;1m";
+        public static String LightYellow = "\u001b[33;1m";
+        public static String LightBlue = "\u001b[34;1m";
+        public static String LightMagenta = "\u001b[35;1m";
+        public static String LightCyan = "\u001b[36;1m";
+        public static String LightWhite = "\u001b[37;1m";
+    }
+    public static class bgColor{
+        public static String Black = "\u001b[40m";
+        public static String Red = "\u001b[41m";
+        public static String Green = "\u001b[42m";
+        public static String Yellow = "\u001b[43m";
+        public static String Blue = "\u001b[44m";
+        public static String Magenta = "\u001b[45m";
+        public static String Cyan = "\u001b[46m";
+        public static String White = "\u001b[47m";
+
+        public static String LightBlack = "\u001b[40;1m";
+        public static String LightRed = "\u001b[41;1m";
+        public static String LightGreen = "\u001b[42;1m";
+        public static String LightYellow = "\u001b[43;1m";
+        public static String LightBlue = "\u001b[44;1m";
+        public static String LightMagenta = "\u001b[45;1m";
+        public static String LightCyan = "\u001b[46;1m";
+        public static String LightWhite = "\u001b[47;1m";
+    }
+    public static String Reset = "\u001b[0m";
+}
