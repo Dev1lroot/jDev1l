6eac400c70e9905d087c621085f21b65c84130c9
Einar Aldersen <46210204+Dev1lroot@users.noreply.github.com>
2019-04-13 14:03:34 +0200

    Create Main.java

---
+++ src/no/dev1lroot/Main.java
@@ -0,0 +1,10 @@
+package no.dev1lroot;
+
+import no.dev1lroot.misc.ui.jDev1l;
+
+public class Main{
+    public static void main(String[] args){
+        jDev1l jd = new jDev1l();
+        jd.Run();
+    }
+}
